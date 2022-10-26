23fdd2d618b9ffec20f1dc2e6bea168c2eb68878
dmilleza <darrenmilleza@gmail.com>
2022-10-25 20:17:50 -0700

    started javascript

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// submit button stuff
+Element.addEventlistener("click," function)
+// local storage set item
+
+// local storage get item
+
+// dark theme
+
+// fetch api
+
+// fetch second api
+
+// make jumboBox disappear
+
+
+
+var jumboBoxEl = document.querySelector(".jumboBox")
+var example1 = document.querySelector("")
+var example1 = document.querySelector("")
+var example1 = document.querySelector("")
+var example1 = document.querySelector("")
+var example1 = document.querySelector("")
+var example1 = document.querySelector("")
+
