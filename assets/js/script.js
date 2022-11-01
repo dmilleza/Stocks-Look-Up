4fec3caa755a76948bdf5b642434662e1bc7f928
dmilleza <darrenmilleza@gmail.com>
2022-10-31 21:15:26 -0700

    first api added

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,19 @@ var navList = document.querySelector('#navbarBasicExample');
 burger.addEventListener('click', () => {
   navList.classList.toggle('is-active');
 });
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'd260607c62msh9aa714a7c4a64dfp1422a2jsn930883be9576',
+    'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
+  },
+};
+
+fetch(
+  'https://yh-finance.p.rapidapi.com/auto-complete?q=tesla&region=US',
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
